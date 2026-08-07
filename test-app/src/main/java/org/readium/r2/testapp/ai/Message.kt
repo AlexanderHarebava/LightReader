bcc54618581007b8e3d7ea8316ee03ffdc5402a8
Alexander Harebava <aharebava5@gmail.com>
2026-08-07 18:52:21 +0300

    Delete test-app directory

--- test-app/src/main/java/org/readium/r2/testapp/ai/Message.kt
+++
@@ -1,9 +0,0 @@
-package org.readium.r2.testapp.ai
-
-data class Message(
-    val text: String,
-    val isFromUser: Boolean,
-    val bookTitle: String? = null,
-    val bookCoverPath: String? = null,
-    val bookId: Long? = null
-)
